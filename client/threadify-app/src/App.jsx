02bf76cbb08af990d76f50740c859e2b2028a5fb
salvo-t <salvot.biz@gmail.com>
2023-03-01 21:04:51 -0500

    Created app.jsx, ChannelContainer.jsx and ChannelListContainer.jsx

--- client/threadify-app/src/App.jsx
+++ client/threadify-app/src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { StreamChat } from 'stream-chat';
+import { ChannelList, Chat } from 'stream-chat-react';
+import { Channel } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+const apiKey = 'n32sfamzsn8a';
+
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div>
-        
+    <div className="app__wrapper">
+        <Chat client={client} theme="team light">
+          <ChannelListContainer
+
+          />
+          <ChannelContainer
+
+          />
+        </Chat>
     </div>
   )
 }
