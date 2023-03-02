05d28a59dd10712f3f8f539db0f88c037646752a
sorryz23 <sorryz23@gmail.com>
2023-03-01 21:57:44 -0500

    fixed typos

--- client/threadify-app/src/components/index.js
+++ client/threadify-app/src/components/index.js
@@ -1,5 +1,3 @@
-719 Bytes
-
 export { default as ChannelContainer } from './ChannelContainer';
 export { default as ChannelListContainer } from './ChannelListContainer';
 export { default as ChannelSearch } from './ChannelSearch';
