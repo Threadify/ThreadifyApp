0ebb9481db7f722de491c8563cf39eb1a5b8e42f
kthom42 <kmgt42@gmail.com>
2023-03-02 20:55:03 -0500

    added authentication code

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,5 @@
-export { default as ChannelContainer } from './ChannelContainer';
-export { default as ChannelListContainer } from './ChannelListContainer';
+export { default as ChannelContainer } from "./ChannelContainer";
+export { default as ChannelListContainer } from "./ChannelListContainer";
 // export { default as ChannelSearch } from './ChannelSearch';
 // export { default as TeamChannelList } from './DirectMessageList';
 // export { default as TeamChannelPreview } from './DirectMessagePreview';
@@ -7,6 +7,6 @@ export { default as ChannelListContainer } from './ChannelListContainer';
 // export { default as CreateChannel } from './CreateChannel';
 // export { default as EditChannel } from './EditChannel';
 // export { default as TeamMessage } from './DirectMessage';
-// export { default as Auth } from './Auth';
+export { default as Auth } from "./Auth";
 // export { default as UserList } from './UserList';
 // export { default as ResultsDropdown } from './ResultsDropdown';
