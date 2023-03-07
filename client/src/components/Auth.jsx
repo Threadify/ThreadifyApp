acd7f055e6d6921214e1f161b71778af2956adce
kthom42 <kmgt42@gmail.com>
2023-03-06 20:08:31 -0500

    created sign up template

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -6,7 +6,12 @@ import signinImage from '../assets/Signin.png';
 
 const Auth = () => {
     const [isSignup, setIsSignup] = useState(false)
+   
     const handleChange = () => {}
+
+    const switchMode = () => {
+        setIsSignup((previousIsSignup) => !previousIsSignup);
+    }
   return (
     <div className='auth_form-container'>
         <div className='auth_form-container_fields'>
@@ -26,8 +31,83 @@ const Auth = () => {
 
                         </div>
                     )}
+                    <div className='auth_form-container_fields-content_input'>
+                            <label htmlFor='username'>Username</label>
+                            <input
+                                name='username'
+                                type='text'
+                                placeholder='UserName'
+                                onChange={handleChange}
+                                required
+                            />
+
+                        </div>
+                        {isSignup && (
+                        <div className='auth_form-container_fields-content_input'>
+                            <label htmlFor ='phoneNumber'>Phone Number</label>
+                            <input
+                                name='phoneNumber'
+                                type='text'
+                                placeholder='Phone Number'
+                                onChange={handleChange}
+                                required
+                            />
+
+                        </div>
+                    )}
+                    {isSignup && (
+                        <div className='auth_form-container_fields-content_input'>
+                            <label htmlFor ='avatarURL'>avatar URL</label>
+                            <input
+                                name='avatarURL'
+                                type='text'
+                                placeholder='Avatar URL'
+                                onChange={handleChange}
+                                required
+                            />
+
+                        </div>
+                    )}
+                    <div className='auth_form-container_fields-content_input'>
+                            <label htmlFor ='password'>Password</label>
+                            <input
+                                name='password'
+                                type='password'
+                                placeholder='Password'
+                                onChange={handleChange}
+                                required
+                            />
+
+                        </div>
+                    {isSignup && (
+                        <div className='auth_form-container_fields-content_input'>
+                            <label htmlFor ='confirmPassword'>Confirm Password</label>
+                            <input
+                                name='confirmPassword'
+                                type='password'
+                                placeholder=' Confirm Password'
+                                onChange={handleChange}
+                                required
+                            />
+
+                        </div>
+                    )}
                 </form>
+                <div className='auth_form-container-fields-account'>
+                    <p>
+                        {isSignup
+                        ?'Already have and account?'
+                        :"Don't have and account"
+                        }
+                        <span onClick={switchMode}>
+                            {isSignup ? 'Sign In' : 'Sign Up'}
+                        </span>
+                    </p>
+                </div>
             </div>
+        </div>
+        <div className='auth_form-container_image'>
+            <img src={signinImage} alt='sign in' />
         </div>
     </div>
   )
