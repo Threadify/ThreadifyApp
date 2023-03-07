d744f814113aeb39f1de95ea5ecf2e32be9a89f0
kthom42 <kmgt42@gmail.com>
2023-03-06 20:53:46 -0500

    fixed syntax errors in channel container

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -28,15 +28,15 @@ const ChannelContainer = ({ isCreating, setIsCreating, isEditing, setIsEditing, 
     )
 
     return (
-        <div className=" channel__container">
-            <Channel
-                EmptyStateIndicator={EmptyState}
-                Message={(messageProps, i) =Simple key={i} {...messageProps} />}
-            >
-                <ChannelInner setIsEditing={setIsEditing} />
-            </Channel>
+        <div className="channel__container">
+          <Channel
+            EmptyStateIndicator={EmptyState}
+            Message={(messageProps, i) => <MessageSimple key={i} {...messageProps} />}
+          >
+            <ChannelInner setIsEditing={setIsEditing} />
+          </Channel>
         </div>
-    );
+      );
 }
 
 export default ChannelContainer;
